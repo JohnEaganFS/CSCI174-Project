870905a28e0ad0f13fefd17e1e4e9633c0168748
John Eagan <johnjeagan@mail.fresnostate.edu>
2021-10-29 16:14:08 -0700

    Editing numpy function typing

--- x.py
+++ x.py
@@ -1,10 +1,10 @@
 import numpy as np
 import timeit
-from multiprocessing import Pool
+from multiprocessing import Pool, cpu_count
 from numba import njit, prange
 
-@njit(parallel=True)#compile function so it runs as machine code
-def MatrixMultiply(A,B,Cores):
+#@njit(parallel=True)#compile function so it runs as machine code
+def MatrixMultiply(A,B,c):
     m,n = A.shape
     m,p = B.shape
     if m <= c: #base case we dont need to partition
@@ -14,29 +14,30 @@ def MatrixMultiply(A,B,Cores):
                 for k in range(n):
                     C[i][j] = C[i][j] + A[i][k]*B[k][j]
         return C
-    Partition(A,B)
+    Partition(A,B,c)
 
-@njit#compile function to run as machine code and not through interpreter
-def Partition(A,B):
+#@njit#compile function to run as machine code and not through interpreter
+def Partition(A,B,c):
     m,n = A.shape
-    n,p = B.shape # should we be verifiying that the A column and the B row are same length instead of assuming
+    n,p = B.shape # should we be verifying that the A column and the B row are same length instead of assuming
     if m <= n:
         #axis 0 = rows, axis 1 = columns
-        A1,A2 = np.split(A, n//2, axis=1)
-        B1,B2 = np.split(B, n//2, axis=0)
-        C = MatrixMultiply(A1,B1)+ MatrixMultiply(A2,B2)
+        [A1,A2] = np.hsplit(A, 2)
+        [B1,B2] = np.vsplit(B, 2)
+        C = MatrixMultiply(A1,B1,c)+ MatrixMultiply(A2,B2,c)
         return C
     else: #m>n
-        A1,A2 = np.split(A, m//2, axis=0)
-        B1,B2 = np.split (B, p//2, axis=1)
-        C1 = MatrixMultiply(A1,B1)
-        C2 = MatrixMultiply(A1,B2)
-        C3 = MatrixMultiply(A2,B1)
-        C4 = MatrixMultiply(A2,B2)
-        C12 = np.append(C1,C2,axis=0) #suposed to be append horizontal. not sure which axis to use
-        c34 = np.append(C3,C4,axis=0)
-        C = np.append(C12,C34,axis=1)
+        [A1,A2] = np.vsplit(A, 2)
+        [B1,B2] = np.hsplit(B, 2)
+        C1 = MatrixMultiply(A1,B1,c)
+        C2 = MatrixMultiply(A1,B2,c)
+        C3 = MatrixMultiply(A2,B1,c)
+        C4 = MatrixMultiply(A2,B2,c)
+        C12 = np.hstack(C1,C2) #supposed to be append horizontal. not sure which axis to use
+        C34 = np.hstack(C3,C4)
+        C = np.vstack(C12,C34)
         return C
+
 def RF(A, B, C):
     for i in range(len(A)):
         for j in range(len(B[0])):
@@ -60,12 +61,16 @@ if __name__ == "__main__":
     copyC = C.copy()
     print("Rows:", row)
     print("Cols:", col)
-    print("Serial:", timeit.timeit('RF(A,B,copyC)', globals=globals(), number=1))
+    cores = cpu_count()
+    result = MatrixMultiply(A,B,cores)
 
-    pool = Pool()
-    rows = len(A)
-    data = [(A, B, C, i) for i in range(rows)]
-    print("Parallel", timeit.timeit('pool.starmap(RFParallel, data)', globals=globals(), number=1))
+
+    #print("Serial:", timeit.timeit('RF(A,B,copyC)', globals=globals(), number=1))
+
+    #pool = Pool()
+    #rows = len(A)
+    #data = [(A, B, C, i) for i in range(rows)]
+    #print("Parallel", timeit.timeit('pool.starmap(RFParallel, data)', globals=globals(), number=1))
     #C = pool.starmap(RFParallel, data)
     #C = [list(array) for array in C]
     #print(C)
