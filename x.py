3406549524d17d2e004efd0212ec02155af5d97e
John Eagan <johnjeagan@mail.fresnostate.edu>
2021-10-29 16:34:24 -0700

    don't worry about it

--- x.py
+++ x.py
@@ -3,45 +3,46 @@ import timeit
 from multiprocessing import Pool, cpu_count
 from numba import njit, prange
 
-#@njit(parallel=True)#compile function so it runs as machine code
+@njit(parallel=True)#compile function so it runs as machine code
 def MatrixMultiply(A,B,c):
     m,n = A.shape
     m,p = B.shape
     if m <= c: #base case we dont need to partition
-        print("Base Case")
+        #print("Base Case")
         C = np.full((m, p), 0)
-        for i in range(m):
+        for i in prange(m):
             for j in range(p):
                 for k in range(n):
                     C[i][j] += A[i][k]*B[k][j]
         return C
-    Partition(A,B,c)
+    return Partition(A,B,c)
 
-#@njit#compile function to run as machine code and not through interpreter
+@njit#compile function to run as machine code and not through interpreter
 def Partition(A,B,c):
     m,n = A.shape
     n,p = B.shape # should we be verifying that the A column and the B row are same length instead of assuming
     if m <= n:
-        print("Case 1")
+        #print("Case 1")
         #axis 0 = rows, axis 1 = columns
         [A1,A2] = np.array_split(A, 2, axis=1)
         [B1,B2] = np.array_split(B, 2, axis=0)
-        print("New Matrices:")
-        print("A1:", A1)
-        print("A2:", A2)
-        print("B1:", B1)
-        print("B2:", B2)
+        #print("New Matrices:")
+        #print("A1:", A1)
+        #print("A2:", A2)
+        #print("B1:", B1)
+        #print("B2:", B2)
         C = MatrixMultiply(A1,B1,c) + MatrixMultiply(A2,B2,c)
+        #print("C?:", C)
         return C
     else: #m>n
-        print("Case 2")
+       # print("Case 2")
         [A1,A2] = np.array_split(A, 2, axis=1)
         [B1,B2] = np.array_split(B, 2, axis=0)
-        print("New Matrices:")
-        print("A1:", A1)
-        print("A2:", A2)
-        print("B1:", B1)
-        print("B2:", B2)
+        #print("New Matrices:")
+       # print("A1:", A1)
+       # print("A2:", A2)
+       # print("B1:", B1)
+       # print("B2:", B2)
         C1 = MatrixMultiply(A1,B1,c)
         C2 = MatrixMultiply(A1,B2,c)
         C3 = MatrixMultiply(A2,B1,c)
@@ -67,7 +68,7 @@ if __name__ == "__main__":
     row = 10
     col = 10
     testNums = [10, 20, 50, 80, 100, 150, 200, 300]
-
+    np.random.seed(42)
     A = np.random.randint(10, size=(row, col))
     B = np.random.randint(10, size=(row, col))
     C = np.full((row, col), 0)
@@ -76,10 +77,10 @@ if __name__ == "__main__":
     print("Cols:", col)
     cores = cpu_count()
     print("Cores:", cores)
-    print("A", A)
-    print("B", B)
+    print("A: \n", A)
+    print("B:", B)
     result = MatrixMultiply(A,B,cores)
-    print(result)
+    print("C:",result)
 
 
     #print("Serial:", timeit.timeit('RF(A,B,copyC)', globals=globals(), number=1))
