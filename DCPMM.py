b994cd9dcc4c3b678d7efb3d76a2983b42ca98fe
John Eagan <johnjeagan@mail.fresnostate.edu>
2021-11-05 20:05:39 -0700

    editing parameters for mm

--- DCPMM.py
+++ DCPMM.py
@@ -7,66 +7,70 @@ from numba import jit, njit, prange, cuda
 from scipy.linalg import blas as FB
 from pynvml import *
 
-def MatrixMultiply(A,B,c,m,n):
+def MatrixMultiply(A,B,c,shape):
+    m = shape[0]
     if m <= c: #base case we dont need to partition
         #if isinstance(A, np.ndarray):
         A = cp.array(A)
         B = cp.array(B)
         return cp.matmul(A,B)
-    return Partition(A,B,c,m,n)
+    return Partition(A,B,c,shape)
 
-def Partition(A,B,c,m,n):
+def Partition(A,B,c,shape):
+    m = shape[0]
     h = nvmlDeviceGetHandleByIndex(0)
     info = nvmlDeviceGetMemoryInfo(h)
     if (20 * m**2 < info.free):
-        return PartitionGPU(cp.array(A),cp.array(B),c,m,n)
+        return PartitionGPU(cp.array(A),cp.array(B),c,shape)
     else:
-        return PartitionCPU(A,B,c,m,n)
+        return PartitionCPU(A,B,c,shape)
 
-def PartitionGPU(A,B,c,m,n):
+def PartitionGPU(A,B,c,shape):
     #print(type(A))
     #print(type(B))
+    m, n = shape
     if m <= n:
         #axis 0 = rows, axis 1 = columns
         [A1,A2] = cp.array_split(A, 2, axis=1)
         [B1,B2] = cp.array_split(B, 2, axis=0)
         #print(type(A1))
-        return MatrixMultiply(A1,B1,c,m,n) + MatrixMultiply(A2,B2,c,m,n)
+        return MatrixMultiply(A1,B1,c,A1.shape) + MatrixMultiply(A2,B2,c,A2.shape)
         #return cp.asnumpy(MatrixMultiply(A1,B1,c) + MatrixMultiply(A2,B2,c))
     else: #m>n
         [A1,A2] = cp.array_split(A, 2, axis=0)
         [B1,B2] = cp.array_split(B, 2, axis=1)
 
-        C1 = MatrixMultiply(A1,B1,c,m,n)
-        C2 = MatrixMultiply(A1,B2,c,m,n)
+        C1 = MatrixMultiply(A1,B1,c,A1.shape)
+        C2 = MatrixMultiply(A1,B2,c,A1.shape)
         C12 = cp.hstack((C1,C2))
         del C1
         del C2
 
-        C3 = MatrixMultiply(A2,B1,c,m,n)
-        C4 = MatrixMultiply(A2,B2,c,m,n)
+        C3 = MatrixMultiply(A2,B1,c,A2.shape)
+        C4 = MatrixMultiply(A2,B2,c,A2.shape)
         C34 = cp.hstack((C3,C4))
         del C3
         del C4
         return cp.vstack((C12,C34))
         #return cp.asnumpy(cp.vstack((C12,C34)))
 
-def PartitionCPU(A,B,c,m,n):
+def PartitionCPU(A,B,c,shape):
+    m, n = shape
     A = cp.asnumpy(A)
     B = cp.asnumpy(B)
     if m <= n:
         #axis 0 = rows, axis 1 = columns
         [A1,A2] = np.array_split(A, 2, axis=1)
         [B1,B2] = np.array_split(B, 2, axis=0)
-        C = cp.asnumpy(MatrixMultiply(A1,B1,c,m,n)) + cp.asnumpy(MatrixMultiply(A2,B2,c,m,n))
+        C = cp.asnumpy(MatrixMultiply(A1,B1,c,A1.shape)) + cp.asnumpy(MatrixMultiply(A2,B2,c,A2.shape))
         return C
     else: #m>n
         [A1,A2] = np.array_split(A, 2, axis=0)
         [B1,B2] = np.array_split(B, 2, axis=1)
-        C1 = cp.asnumpy(MatrixMultiply(A1,B1,c,m,n))
-        C2 = cp.asnumpy(MatrixMultiply(A1,B2,c,m,n))
-        C3 = cp.asnumpy(MatrixMultiply(A2,B1,c,m,n))
-        C4 = cp.asnumpy(MatrixMultiply(A2,B2,c,m,n))
+        C1 = cp.asnumpy(MatrixMultiply(A1,B1,c,A1.shape))
+        C2 = cp.asnumpy(MatrixMultiply(A1,B2,c,A1.shape))
+        C3 = cp.asnumpy(MatrixMultiply(A2,B1,c,A2.shape))
+        C4 = cp.asnumpy(MatrixMultiply(A2,B2,c,A2.shape))
         
         C12 = np.hstack((C1,C2)) #supposed to be append horizontal. not sure which axis to use
         C34 = np.hstack((C3,C4))
@@ -83,8 +87,8 @@ def cupyMult(A,B):
 
 if __name__ == "__main__":
     nvmlInit()
-    row = 30000
-    col = 30000
+    row = 20000
+    col = 20000
     testNums = [10, 20, 50, 80, 100, 150, 200, 300]
     np.random.seed(42)
 
@@ -99,7 +103,7 @@ if __name__ == "__main__":
     print("B:\n", B)
 
     start = time.time()
-    result = MatrixMultiply(A,B,row/cores,row,col)
+    result = MatrixMultiply(A,B,row/cores,(row,col))
     end = time.time()
     print("C:\n",result)
     print("Our Algorithm")
